420096be7c5ab5ab23984ed1fbe36ec1a724454d
Elisha <elishajp20@gmail.com>
2019-08-14 23:32:25 -0500

    Initial form

--- src/Signup.js
+++ src/Signup.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import useSignUpForm from './CustomHooks';
+import React, { useState } from "react";
+import useSignUpForm from "./CustomHooks";
 
 const Signup = () => {
   const signup = () => {
     alert(`User Created! 
 Name: ${inputs.firstName} ${inputs.lastName}
 Email: ${inputs.email}`);
-  }
-  const {inputs, handleInputChange, handleSubmit} = useSignUpForm({firstName: '', lastName: '', email: '', password1: '', password2: ''}, signup);
+  };
+  const { inputs, setInputs, handleInputChange, handleSubmit } = useSignUpForm(
+    {
+      fullName: "",
+      email: "",
+      phonenumber: "",
+      zipcode: "",
+      yes: "",
+      no: "",
+      password1: "",
+      password2: ""
+    },
+    signup
+  );
+
   return (
     <div className="section is-fullheight">
       <div className="container">
@@ -15,42 +28,97 @@ Email: ${inputs.email}`);
           <div className="box">
             <form onSubmit={handleSubmit} autoComplete="off">
               <div className="field">
-                <label className="label has-text-centered">First Name</label>
+                <label className="label has-text-centered">Full Name</label>
                 <div className="control">
-                  <input className="input" type="text" name="firstName" onChange={handleInputChange} value={inputs.firstName} required />
+                  <input
+                    className="input"
+                    type="text"
+                    name="fullName"
+                    onChange={handleInputChange}
+                    value={inputs.fullName}
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="field">
+                <label className="label has-text-centered">Email</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="email"
+                    name="email"
+                    onChange={handleInputChange}
+                    value={inputs.email}
+                    required
+                  />
                 </div>
               </div>
               <div className="field">
-                <label className="label has-text-centered">Last Name</label>
+                <label className="label has-text-centered">Phone #</label>
                 <div className="control">
-                  <input className="input" type="text" name="lastName" onChange={handleInputChange} value={inputs.lastName} required />
+                  <input
+                    className="input"
+                    type="text"
+                    name="phonenumber"
+                    onChange={handleInputChange}
+                    value={inputs.lastName}
+                    required
+                  />
                 </div>
               </div>
+
               <div className="field">
-                <label className="label has-text-centered">Email Address</label>
+                <label className="label has-text-centered">Zip Code</label>
                 <div className="control">
-                  <input className="input" type="email" name="email" onChange={handleInputChange} value={inputs.email} required />
+                  <input
+                    className="input"
+                    type="text"
+                    name="zipcode"
+                    onChange={handleInputChange}
+                    value={inputs.lastName}
+                    required
+                  />
                 </div>
               </div>
-              <div className="field">
-                <label className="label has-text-centered">Password</label>
-                <div className="control">
-                  <input className="input" type="password" name="password1" onChange={handleInputChange} value={inputs.password1}/>
-                </div>
+
+              <div className="form-check">
+                <label>
+                  <p>Did the chicken come before the egg?</p>
+                  <input
+                    type="radio"
+                    name="yes"
+                    value={inputs.yes}
+                    className="form-check-input"
+                  />
+                  Yes
+                </label>
               </div>
-              <div className="field">
-                <label className="label has-text-centered">Re-enter Password</label>
-                <div className="control">
-                  <input className="input" type="password" name="password2" onChange={handleInputChange} value={inputs.password2}/>
-                </div>
+
+              <div className="form-check">
+                <label>
+                  <input
+                    type="radio"
+                    name="no"
+                    value={inputs.no}
+                    className="form-check-input"
+                  />
+                  No
+                </label>
               </div>
-              <button className="button is-block is-fullwidth is-success" type="submit">Sign Up</button>
+
+              <button
+                className="button is-block is-fullwidth is-success"
+                type="submit"
+              >
+                Sign Up
+              </button>
             </form>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Signup;
