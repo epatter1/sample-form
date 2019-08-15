5d4bdda541bbe6c55cb7eb44bcfb77a4e6605188
Elisha <elishajp20@gmail.com>
2019-08-15 01:53:31 -0500

    Added multi-select dropdown

--- src/Signup.js
+++ src/Signup.js
@@ -1,7 +1,58 @@
 import React, { useState } from "react";
 import useSignUpForm from "./CustomHooks";
 
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  Checkbox,
+  MenuItem,
+  ListItemText,
+  withStyles,
+  makeStyles,
+  Input
+} from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    maxWidth: 300
+  },
+  chips: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  chip: {
+    margin: 2
+  },
+  noLabel: {
+    marginTop: theme.spacing(3)
+  }
+}));
+
+const names = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
 const Signup = () => {
+  const classes = useStyles();
+  const [personName, setPersonName] = React.useState([]);
+
+  function handleChange(event) {
+    setPersonName(event.target.value);
+  }
+
   const signup = () => {
     alert(`User Created! 
 Name: ${inputs.firstName} ${inputs.lastName}
@@ -105,6 +156,27 @@ Email: ${inputs.email}`);
                   />
                   No
                 </label>
+              </div>
+              <div className="form-check">
+              <FormControl className={classes.formControl}>
+                <label>Favorite days of the week: </label>
+                <InputLabel htmlFor="select-multiple-checkbox"></InputLabel>
+                <Select
+                  multiple
+                  value={personName}
+                  onChange={handleChange}
+                  input={<Input id="select-multiple-checkbox" />}
+                  renderValue={selected => selected.join(", ")}
+                  withStyles={withStyles}
+                >
+                  {names.map(name => (
+                    <MenuItem key={name} value={name}>
+                      <Checkbox checked={personName.indexOf(name) > -1} />
+                      <ListItemText primary={name} />
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
               </div>
 
               <button
